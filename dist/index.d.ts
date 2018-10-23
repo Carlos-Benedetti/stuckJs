e358255fa21db3f3590c59498e434b76cffa477c
ciag <carlos.lopes@ciag.org.br>
2018-10-23 09:49:42 -0300

    add jsDoc

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,18 @@
+/**
+ * <b> Creates a two way bind with the elements that have the stuk attribute value equal to a variable name in the object sent as parameter e.g : <input stuk="name"> by having the "name" as attribute the value of that input in linked to the variable "name" sent with the object </b>
+ * @example
+ * // returns undefined
+ * <input stuk="name"> 
+ * <script>
+ * stuk.bindPage({name:"Benedetti"})
+ * </script>
+ *
+ * @param {object} (your page variables object)
+ * 
+ * @fileOverview simple two way data bind for js.
+ * @author <a href="https://gitlab.com/Cadub">Carlos Benedetti</a>
+ * @version 1.0.1
+ */
 export declare function bindPage(page: object): void;
+
+ 
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,23 @@
+/**
+ * @fileOverview simple two way data bind for js.
+ * @author <a href="https://gitlab.com/Cadub">Carlos Benedetti</a>
+ * @version 0.0.1
+ */
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var binding = require("./Binding");
-console.log(binding)
+    /**
+	 * <b> Creates a two way bind with the elements that have the stuk attribute value equal to a variable name in the object sent as parameter e.g : <input stuk="name"> by having the "name" as attribute the value of that input in linked to the variable "name" sent with the object </b>
+     * @example
+     * // returns undefined
+     * <input stuk="name"> 
+     * <script>
+     * stuk.bindPage({name:"Benedetti"})
+     * </script>
+	 *
+	 * @param {object} (all your page variables)
+	 * 
+	 */
 function bindPage(page) {
     var input = document.querySelectorAll('[stuk]');
     input.forEach(element =>{
