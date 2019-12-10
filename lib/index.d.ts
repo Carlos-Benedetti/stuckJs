f998c25b2fc77c970569ba7b4f7169408b554a4d
Carlos Benedetti <cbjrcadu@gmail.com>
2019-12-10 10:48:35 -0300

    0.1.2

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,17 +1,20 @@
 /**
  * @fileOverview simple two way data bind for js.
  * @author <a href="https://gitlab.com/Cadub">Carlos Benedetti</a>
- * @version 0.0.1
+ * @version 0.1.2
  */
 /**
- * <b> Creates a two way bind with the elements that have the stuk attribute value equal to a variable name in the object sent as parameter e.g : <input stuk="name"> by having the "name" as attribute the value of that input in linked to the variable "name" sent with the object </b>
+ * Creates a two way bind with the elements that have the stuk attribute value equal to a variable name in the object sent as parameter e.g : ` <input stuk="name">` by having the "name" as attribute the value of that input in linked to the variable "name" sent with the object
+ *
  * @example
+ * ```html
  * <input stuk="name">
  * <script>
  *      stuk.bindPage({name:"Benedetti"})
  * </script>
+ * ```
  *
- * @param {object} (all your page variables)
+ *
  *
  */
 declare module Stuk {
